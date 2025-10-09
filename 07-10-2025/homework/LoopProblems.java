bf27aa2e6ba2cc7facf5ed70918990ea8552e135
PremK177 <prem.nampalliwar177@gmail.com>
2025-10-09 11:20:23 +0530

    Completing homework of day2

--- 07-10-2025/homework/LoopProblems.java
+++ 07-10-2025/homework/LoopProblems.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class LoopProblems {
 
     public static void main(String[] args) {
-
+    printNumbers();
+    multiplicationTable();
+    factorial();
+    sumOfNaturalNumbers();
+    fibonacciSeries();
     }
 
     public static void printNumbers() {
@@ -25,7 +29,7 @@ public class LoopProblems {
 
     public static void factorial() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number to find result of: ");
+        System.out.println("Enter a number to find factorial of: ");
         int num = Integer.parseInt(scan.nextLine());
         int result = 1;
         for (int i = 1; i <= num; i++) {
@@ -33,6 +37,30 @@ public class LoopProblems {
         }
         System.out.println("Factorial of " + num + " is " + result);
 
+    }
+
+    public static void sumOfNaturalNumbers() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first 10 natural numbers = " + sum);
+    }
+
+    public static void fibonacciSeries() {
+        Scanner scan = new Scanner(System.in);
+        int a = 0;
+        int b = 1;
+        System.out.println("Enter number of terms of fibonacci series to print:");
+        int c = Integer.parseInt(scan.nextLine());
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 2; i < c; i++) {
+            int nextTerm = a + b;
+            System.out.println(nextTerm);
+            a = b;
+            b = nextTerm;
+        }
     }
 
 }
